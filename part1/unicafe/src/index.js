3e90cb211b71a45c7a08c951e63260c91a4d89c9
Esteve2604 <estebanasperuiz@gmail.com>
2023-02-04 17:26:46 +0100

    Terminado Unicafe :)

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,31 +1,37 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Statistics = ({ good, neutral, bad, all }) => {
-
-  <Statistics props={({ good, neutral, bad, all })} />
+const Statistics = (props) => {
+  if (props.all == 0 && props.text.localeCompare("good") == 0) {
+    return (
+      <>
+        <h1>No feedback given</h1>
+      </>
+    )
+  } else if (props.all == 0) return
   return (
     <>
       <table>
         <tbody>
-          <tr>good <td> {good} </td> </tr>
-          <tr>neutral <td>{neutral}</td></tr>
-          <tr>bad <td>{bad}</td></tr>
-          <tr>all <td>{all}</td></tr>
-          <tr>average <td>{(good - bad) / all}</td></tr>
-          <tr>positive <td>{good / all}%</td></tr>
+          <tr><td>{props.text}</td>
+            <td>{props.value}</td></tr>
         </tbody>
       </table>
     </>
   )
 }
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  let all = 0
-  const setToValue = (value, set) => () => (set(value))
+  let all = good + bad + neutral
+  let average = (good - bad) / all
+  let positive = (good / all)
+  const setToValue = (value, set) => () => {
+    return (set(value))
+  }
   return (
     <>
       <h1>give feedback</h1>
@@ -33,7 +39,12 @@ const App = () => {
       <button onClick={setToValue(neutral + 1, setNeutral)}>neutral</button>
       <button onClick={setToValue(bad + 1, setBad)}>bad</button>
       <h1>statistics</h1>
-      <Statistics props={({ good, neutral, bad, all })} />
+      <Statistics text="good" value={good} all={all} />
+      <Statistics text="neutral" value={neutral} all={all} />
+      <Statistics text="bad" value={bad} all={all} />
+      <Statistics text="all" value={all} all={all} />
+      <Statistics text="average" value={`${average}`} all={all} />
+      <Statistics text="positive" value={`${positive}%`} all={all} />
     </>
   )
 }
